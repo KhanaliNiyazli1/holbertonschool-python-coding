7a78b67a3f4dd760f0fb179a50e0d859a9fd0a2a
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-19 17:57:21 +0400

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
     arguments = sys.argv[1:]
 
     if len(arguments) == 0:
         print("0 arguments.")
-    elif len(arguments) == 1:
-        print("1 argument: ")
-        for index, arg in arguments:
-            print("{}: {}".format(index, arg))
     else:
-        for index, arg in arguments:
-            print("{} arguments:".format(index))
-            print("{}: {}".format(index, arg))
+        print(f"{len(arguments)} argument{'s' if len(arguments) > 1 else ''}:")
+
+        for index, arg in enumerate(arguments, start=1):
+            print(f"{index}: {arg}")
