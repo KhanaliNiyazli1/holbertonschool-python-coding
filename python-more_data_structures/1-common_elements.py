bb8a6956e41c3b4ad838fbe1c0db30dd60577fe6
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2024-11-20 15:53:22 +0400

    task1

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     result = set()
-    for i in set_1:
-        for j in set_2:
-                if i == j:
-                    result.add(i)
+    for item in set_1:
+        if item in set_2:
+            result.add(item)
     return result
